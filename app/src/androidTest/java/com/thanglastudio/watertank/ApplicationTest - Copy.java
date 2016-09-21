6f6bf5e52e3c31c661cc54cc887c5746351245ba
BishwajitNepali <BishwajitNepali@users.noreply.github.com>
2016-09-20 22:15:50 -0500

    US98484-ApplicationTest-Copy.java/com/thanglastudio/watertank/ApplicationTest - Copy.java Added

---
+++ app/src/androidTest/java/com/thanglastudio/watertank/ApplicationTest - Copy.java
@@ -0,0 +1,10 @@
+/* Test */package com.thanglastudio.watertank;
+
+import /**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
